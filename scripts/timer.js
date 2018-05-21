a6eef51d86326d3b9bb170a51d31d5e5ba621e1f
sagago <steven.a.gago@gmail.com>
2018-05-21 12:00:24 -0500

    Converted timer.js to TypeScript
    Converted timer.js to timer.ts.  Intialized a new, better commented tsconfig.json file.

--- scripts/timer.js
+++ scripts/timer.js
@@ -2,15 +2,14 @@
 /**
  * Raises an onElapsed event after a specified number of milliseconds.
  */
-var Timer = /** @class */ (function () {
+class Timer {
     /**
      * Instantiates a new instance of Timer.
      * @param duration Length of the timer in milliseconds.
-     * @param onElapse Callback method called when the timer finishes.
+     * @param onElapseHandler Callback method called when the timer finishes.
      * @param autoStart True to start the timer immediately; otherwise, false.
      */
-    function Timer(duration, onElapse, autoStart) {
-        if (autoStart === void 0) { autoStart = true; }
+    constructor(duration, onElapseHandler, autoStart = true) {
         /**
          * The timer's unique ID returned from the window.setTimeout method.
          */
@@ -21,34 +20,122 @@ var Timer = /** @class */ (function () {
          */
         this.duration = 0;
         /**
+         * The start time of this timer instance.
+         */
+        this.startTime = null;
+        /**
+         * The remaining time left for this timer instance before elapsing.
+         */
+        this.remainingTime = null;
+        /**
          * Callback method that is called when the timer finishes.
          */
-        this.onElapse = null;
-        this.startTime = null;
-        this.remainingTime = null;
-        this.duration = duration;
-        this.onElapse = onElapse;
+        this.onElapseHandler = null;
+        this.Duration = duration >= 0 ? duration : 0;
+        this.OnElapseHandler = onElapseHandler;
+        // Should we start the timer immediately?
         if (autoStart) {
             this.start();
         }
     }
     /**
+     * Gets this timer's unique ID.
+     */
+    get TimerID() {
+        return this.timerId;
+    }
+    /**
+     * Sets this timer's unique ID.
+     */
+    set TimerID(timerId) {
+        this.timerId = timerId;
+    }
+    /**
+     * Gets the duration of this timer instance in milliseconds.
+     */
+    get Duration() {
+        return this.duration;
+    }
+    /**
+     * Sets the duration of this timer instance in milliseconds.
+     */
+    set Duration(duration) {
+        this.duration = duration;
+    }
+    /**
+     * Gets this timer's start time.
+     */
+    get StartTime() {
+        return this.startTime;
+    }
+    /**
+     * Sets this timer's start time.
+     */
+    set StartTime(startTime) {
+        this.startTime = startTime;
+    }
+    /**
+     * Gets the remaining time before this timer instance elapses.
+     */
+    get RemainingTime() {
+        return this.remainingTime;
+    }
+    /**
+     * Sets the remaining time before this timer instance elapses.
+     */
+    set RemainingTime(remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+    /**
+     * Gets the callback method for this timer instance.
+     */
+    get OnElapseHandler() {
+        return this.onElapseHandler;
+    }
+    /**
+     * Sets the callback method for this timer instance.
+     */
+    set OnElapseHandler(onElapseHandler) {
+        this.onElapseHandler = onElapseHandler;
+    }
+    getDuration() {
+        return this.Duration;
+    }
+    getRemainingTime() {
+        return Math.max(this.RemainingTime.getTime() - new Date().getTime() - this.StartTime.getTime(), 0);
+    }
+    /**
      * Starts this timer instance.
      */
-    Timer.prototype.start = function () {
-        this.startTime = new Date();
-        window.clearTimeout(this.timerId);
-        this.timerId = window.setTimeout(this.onElapse, this.remainingTime);
-    };
+    start() {
+        window.clearTimeout(this.TimerID);
+        this.StartTime = new Date();
+        if (this.RemainingTime == null) {
+            this.RemainingTime = new Date(this.StartTime.getTime() + this.Duration);
+        }
+        this.TimerID = window.setTimeout(this.OnElapseHandler, this.StartTime.getTime());
+    }
+    /**
+     * Pauses this timer instance.  One can resume the timer by calling
+     * the start method.
+     */
+    pause() {
+        window.clearTimeout(this.TimerID);
+        this.RemainingTime.setTime(this.getRemainingTime());
+    }
     /**
      * Stops this timer instance.
      */
-    Timer.prototype.stop = function () {
-        window.clearTimeout(this.timerId);
-        this.remainingTime.setTime(this.remainingTime.getTime() -
-            new Date().getTime() -
-            this.startTime.getTime());
-    };
-    return Timer;
-}());
+    stop() {
+        window.clearTimeout(this.TimerID);
+        this.RemainingTime = null;
+    }
+    /**
+     * Resets this timer instance.
+     */
+    reset() {
+        this.stop();
+        this.start();
+    }
+}
 //# sourceMappingURL=timer.js.map
--- scripts/timer.ts
+++ scripts/timer.ts
@@ -15,48 +15,173 @@ class Timer {
   private duration: number = 0;
 
   /**
-   * Callback method that is called when the timer finishes.
+   * The start time of this timer instance.
    */
-  private onElapse: (this:void) => void = null;
-
   private startTime: Date = null;
 
+  /**
+   * The remaining time left for this timer instance before elapsing.
+   */
   private remainingTime: Date = null;
+
+  /**
+   * Callback method that is called when the timer finishes.
+   */
+  private onElapseHandler: (this: void) => void = null;
+
+  /**
+   * Gets this timer's unique ID.
+   */
+  private get TimerID(): number {
+
+    return this.timerId;
+  }
+
+  /**
+   * Sets this timer's unique ID.
+   */
+  private set TimerID(timerId: number) {
+
+    this.timerId = timerId;
+  }
+
+  /**
+   * Gets the duration of this timer instance in milliseconds.
+   */
+  private get Duration(): number {
+
+    return this.duration;
+  }
+
+  /**
+   * Sets the duration of this timer instance in milliseconds.
+   */
+  private set Duration(duration: number) {
+
+    this.duration = duration;
+  }
+
+  /**
+   * Gets this timer's start time.
+   */
+  private get StartTime(): Date {
+
+    return this.startTime;
+  }
+
+  /**
+   * Sets this timer's start time.
+   */
+  private set StartTime (startTime: Date) {
+
+    this.startTime = startTime;
+  }
+
+  /**
+   * Gets the remaining time before this timer instance elapses.
+   */
+  private get RemainingTime(): Date {
+
+    return this.remainingTime;
+  }
+
+  /**
+   * Sets the remaining time before this timer instance elapses.
+   */
+  private set RemainingTime(remainingTime: Date) {
+
+    this.remainingTime = remainingTime;
+  }
+
+  /**
+   * Gets the callback method for this timer instance.
+   */
+  private get OnElapseHandler(): (this:void) => void {
+
+    return this.onElapseHandler;
+  }
+
+  /**
+   * Sets the callback method for this timer instance.
+   */
+  private set OnElapseHandler(onElapseHandler: (this:void) => void ) {
+
+    this.onElapseHandler = onElapseHandler;
+  }
+
 
   /**
    * Instantiates a new instance of Timer.
    * @param duration Length of the timer in milliseconds.
-   * @param onElapse Callback method called when the timer finishes.
+   * @param onElapseHandler Callback method called when the timer finishes.
    * @param autoStart True to start the timer immediately; otherwise, false.
    */
   constructor (duration: number,
-               onElapse: (this: void) => void,
+               onElapseHandler: (this: void) => void,
                autoStart: boolean = true) {
 
-    this.duration = duration;
-    this.onElapse = onElapse;
+    this.Duration = duration >= 0 ? duration : 0;
+    this.OnElapseHandler = onElapseHandler;
 
+    // Should we start the timer immediately?
     if (autoStart) {
         this.start();
     }
+  }
+
+  public getDuration(): number {
+    return this.Duration;
+  }
+
+  public getRemainingTime(): number {
+
+    return Math.max(this.RemainingTime.getTime() - new Date().getTime() - this.StartTime.getTime(), 0);
   }
 
   /**
    * Starts this timer instance.
    */
   public start() {
-    this.startTime = new Date();
-    window.clearTimeout(this.timerId);
-    this.timerId = window.setTimeout(this.onElapse, this.remainingTime);
+    
+    window.clearTimeout(this.TimerID);
+
+    this.StartTime = new Date();
+
+    if (this.RemainingTime == null) {
+
+      this.RemainingTime = new Date(this.StartTime.getTime() + this.Duration);
+    }
+
+    this.TimerID = window.setTimeout(this.OnElapseHandler, this.StartTime.getTime())
+  }
+
+  /**
+   * Pauses this timer instance.  One can resume the timer by calling
+   * the start method.
+   */
+  public pause() {
+
+    window.clearTimeout(this.TimerID);
+
+    this.RemainingTime.setTime(this.getRemainingTime());
   }
 
   /**
    * Stops this timer instance.
    */
   public stop() {
-    window.clearTimeout(this.timerId);
-    this.remainingTime.setTime(this.remainingTime.getTime() -
-                                new Date().getTime() -
-                                this.startTime.getTime());
+
+    window.clearTimeout(this.TimerID);
+
+    this.RemainingTime = null;
+  }
+
+  /**
+   * Resets this timer instance.
+   */
+  public reset() {
+
+    this.stop();
+    this.start();
   }
 }
